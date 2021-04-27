3eb01c5b971a059f9c47e4fab7d4bfaf188cf659
bluebayani <bayaniblue@gmail.com>
2021-04-27 12:18:11 -0700

    Updates to capability 5

--- capabilities/capability_five.py
+++ capabilities/capability_five.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import os
 import random
 import string
+import re
 from tkinter import filedialog
 from tkinter import *
 from PIL import Image, ImageTk, ImageOps
@@ -13,8 +14,8 @@ class CapabilityFive:
     def __init__(self, frame, guest_id):
         self.frame = frame
         self.guests = get_guests()
-        self.current_guestid = guest_id
         self.current_guest = find_guest(guest_id)
+        self.current_guestid = guest_id
         self.filename = self.current_guest.get_img_path()
 
         profile_title = tk.Label(
@@ -103,17 +104,50 @@ class CapabilityFive:
         self.vehicle_field.insert(0, self.current_guest.get_vehicle())
         self.populate_img(self.current_guest.get_img_path())
 
+    def valid_number(self, phone_number):
+        if len(phone_number) != 12:
+            return False
+        for i in range(12):
+            if i in [3, 7]:
+                if phone_number[i] != '-':
+                    return False
+            elif not phone_number[i].isalnum():
+                return False
+        return True
+
+    def valid_email(self, email):
+        regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+        if(re.search(regex, email)):
+            return True
+        else:
+            return False
+
+    def popup_msg(self, msg):
+        popup = tk.Tk()
+        popup.wm_title("Error: Unable to save changes")
+        label = tk.Label(popup, text=msg)
+        label.pack(side="top", fill="x", padx=20, pady=10)
+        B1 = tk.Button(popup, text="Okay", command=popup.destroy)
+        B1.pack()
+        popup.mainloop()
+
     def save_changes(self):
         fn = os.path.basename(self.filename)
         file_path = os.path.join(r"./res/", fn)
-
+        phone_number = self.phone_field.get()
+        email = self.email_field.get()
         if not os.path.exists(file_path):
             self.filename = self.save_img()
-        changed_fields = [self.fname_field.get(), self.lname_field.get(), self.phone_field.get(), self.address_field.get(),
-                          self.email_field.get(), self.id_field.get(), self.vehicle_field.get(), self.filename]
-        save_label = tk.Label(
-            self.frame, text="Changes saved to database.").grid(row=11, column=0, padx=15, pady=5)
-        update_guest(self.current_guestid, changed_fields)
 
-        if self.current_guestid != self.id_field.get():
-            self.current_guestid = self.id_field.get()
+        if not self.valid_number(phone_number) or not self.valid_email(email):
+            error_msg = "Phone number must be in the format: ###-###-####\nand Email must be in the format: example@email.com"
+            self.popup_msg(error_msg)
+        else:
+            changed_fields = [self.fname_field.get(), self.lname_field.get(), self.phone_field.get(), self.address_field.get(),
+                              self.email_field.get(), self.id_field.get(), self.vehicle_field.get(), self.filename]
+            message_label = tk.Label(
+                self.frame, text="Changes saved to database.").grid(row=11, column=0, padx=15, pady=5)
+            update_guest(self.current_guestid, changed_fields)
+
+            if self.current_guestid != self.id_field.get():
+                self.current_guestid = self.id_field.get()
