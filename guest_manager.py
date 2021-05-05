abd4613672d9ef80456bedff05b8103909a00042
bluebayani <bayaniblue@gmail.com>
2021-05-04 18:06:16 -0700

    Handles empty string

--- guest_manager.py
+++ guest_manager.py
@@ -79,7 +79,9 @@ def search_guests(flg, key):
     results = []
     guests = get_guests_search()
     key = key.upper()
-    if flg == "Exact":
+    if key == "":
+        return results
+    elif flg == "Exact":
         for guest in guests:
             if guest.get_fname() == key or guest.get_lname() == key or guest.get_rm_number() == key or guest.get_phone() == key or guest.get_address() == key or guest.get_chk_in() == key or guest.get_chk_out() == key:
                 results.append(guest)
